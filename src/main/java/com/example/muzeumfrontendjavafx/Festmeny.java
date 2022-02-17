4fff503e6d9727ceb0d2aecaa2b9bf0e91d6d686
Domokos David <domokosdavid4@gmail.com>
2022-02-17 16:21:23 +0100

    Szobor,festmeny classban privat valtozok

--- src/main/java/com/example/muzeumfrontendjavafx/Festmeny.java
+++ src/main/java/com/example/muzeumfrontendjavafx/Festmeny.java
@@ -1,4 +1,8 @@
 package com.example.muzeumfrontendjavafx;
 
 public class Festmeny {
+    private int id;
+    private String cim;
+    private int ev;
+    private int kiallitva;
 }
--- src/main/java/com/example/muzeumfrontendjavafx/Szobor.java
+++ src/main/java/com/example/muzeumfrontendjavafx/Szobor.java
@@ -1,4 +1,8 @@
 package com.example.muzeumfrontendjavafx;
 
 public class Szobor {
+    private int id;
+    private String szemely;
+    private int magassag;
+    private int ar;
 }
