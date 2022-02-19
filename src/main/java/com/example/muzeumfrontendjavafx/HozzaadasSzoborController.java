b1bb3a24b433db53add612397a1b563e9125e2a6
Domokos David <domokosdavid4@gmail.com>
2022-02-19 21:03:07 +0100

    hozzaadasszobor-view id-k,controllerben letrehozva

--- src/main/java/com/example/muzeumfrontendjavafx/HozzaadasSzoborController.java
+++ src/main/java/com/example/muzeumfrontendjavafx/HozzaadasSzoborController.java
@@ -1,4 +1,18 @@
 package com.example.muzeumfrontendjavafx;
 
-public class HozzaadasSzoborController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class HozzaadasSzoborController{
+    @FXML
+    private TextField inputSzemely;
+    @FXML
+    private Spinner<Integer> inputAr, inputMagassag;
+
+    @FXML
+    public void buttonHozzaad(ActionEvent actionEvent) {
+
+    }
 }
